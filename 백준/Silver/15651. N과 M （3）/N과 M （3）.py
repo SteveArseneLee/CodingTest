c284fbd97adae7adfef39c1c9326bff439bd18fb
SquidEngineer <lclgood@khu.ac.kr>
2023-08-27 11:29:46 +0900

    [Silver III] Title: N과 M (3), Time: 584 ms, Memory: 138244 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.py
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -1,5 +1,6 @@
 import sys
 si = sys.stdin.readline
+print = sys.stdout.write
 n, m = map(int, si().split())
 
 selected = [0 for _ in range(m)]
@@ -8,8 +9,8 @@ used = [0 for _ in range(n+1)]
 def rec_func(k):
     if k == m:
         for x in selected:
-            sys.stdout.write(str(x) + ' ')
-        sys.stdout.write('\n')
+            print(str(x) + ' ')
+        print('\n')
     else:
         for cand in range(1,n+1):
             selected[k] = cand
