c35f0ec0a1a10a322996d708d6670616cd6e4e77
SquidEngineer <lclgood@khu.ac.kr>
2023-08-27 11:24:20 +0900

    [Silver III] Title: N과 M (3), Time: 1088 ms, Memory: 276736 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,12 @@
+def dfs(n, lst):
+    if n == M: # 종료조건
+        ans.append(lst)
+        return
+    for j in range(1, N+1):
+        dfs(n+1, lst+[j])
+
+N, M = map(int, input().split())
+ans = []
+dfs(0, [])
+for lst in ans:
+    print(*lst)
